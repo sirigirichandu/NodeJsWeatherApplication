4d5f885b5d859fec60c02b56080877fccfdad90d
sirigirichandu <chandu.sirigiri@gmail.com>
2019-04-06 15:21:19 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
